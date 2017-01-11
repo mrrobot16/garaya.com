f4300eca7d69f12eea8febfbe276fb3d6a82b24f
= <hmgaar@gmail.com>
2017-01-11 15:05:07 -0500

    change landing to index, added favico, added open node_module

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
+const open = require('open');
 var app = express();
 var port = process.env.PORT || 5000
 // // expose node_modules to client app
 app.use(express.static(__dirname + "/node_modules"));
 app.use(express.static(__dirname + "/assets"));
-// app.use(express.static(path.join(__dirname, '/assets')));
 app.get('/', function(req, res) {
-        res.sendfile('./landing.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 app.listen(port);
+console.log("Go to localhost:5000");
+open("http://localhost:5000");
 module.exports = app;
